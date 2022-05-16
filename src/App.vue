0c4619d5503d0face18219b62f87f5b5c8877de5
Frank3TheTank3 <francisnic90@gmail.com>
2022-05-16 09:31:28 +0200

    MainCommitWelcomeScreenStart_1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo_op.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <ul class="listElement">
+        <li class="center">Coffee</li>
+        <li class="center">Tea</li>
+        <li class="center">Milk</li>
+      </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
@@ -25,4 +25,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+
+.listElement li {
+  list-style-type: none;
+  background-color: rgb(54, 51, 120);
+  font-size: 30px;
+  color: chocolate;
+  margin: 50px;
+  line-height: 150px;
+}
+
+
 </style>
