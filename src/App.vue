e6846eee1fb34e6741a75d78ca26d466c9081cbf
Frank3TheTank3 <francisnic90@gmail.com>
2022-05-16 15:41:58 +0200

    FinalFixCommitWelcomeList

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
   <div id="app" class="center">
     <!--Main Container-->
     <div id="maincontainer">
-
       <!--Welcome & Title-->
       <div class="welcome">
         <h1 class="welcometext">Welcome to</h1>
@@ -11,24 +10,14 @@
       </div>
 
       <!--Date-->
-      <h1 class="date">{{ date }}</h1>
+      <h1 class="date">{{date}}</h1>
 
       <!--Main Activity List-->
       <ul class="list">
-        <li class="center">
-          <p class="fat">14 00 Uhr 07.08.2021</p>
-          <p class="title">Basisbeschäftigung Besuch</p>
-          <p>Interessierte für den zweiten Kurs werden uns besuchen</p>
-        </li>
-        <li class="center">
-          <p class="fat">15 30 Uhr 09.08.2021</p>
-          <p class="title">Firmenbesuch Microsoft</p>
-          <p>Roger Halbheer, Head of Security</p>
-        </li>
-        <li class="center">
-          <p class="fat">8 30 Uhr 10.08.2021</p>
-          <p class="title">Tugce Nur returns to Office</p>
-          <p>We will continue working on this app together on Tuesday!</p>
+        <li class="center" v-for="post in allPosts" :key="post">
+          <p class="fat">{{post.postDate}}</p>
+          <p class="title">{{post.postEvent}}</p>
+          <p>{{post.postDes}}</p>
         </li>
       </ul>
     </div>
@@ -42,18 +31,74 @@
   </div>
 </template>
 
+<!--Scripts
+
+export default {
+  name: "OpportunityWelcomeScreen",
+
+  data: {
+    allPosts: [
+      {
+        postDate: "14 00 Uhr 07.08.2021",
+        postEvent: "Basisbeschäftigung Besuch",
+        postDes: "Interessierte für den zweiten Kurs werden uns besuchen",
+      },
+      {
+        postDate: "15 30 Uhr 09.08.2021",
+        postEvent: "Firmenbesuch Microsoft",
+        postDes: "Roger Halbheer, Head of Security",
+      },
+      {
+        postDate: "8 30 Uhr 10.08.2021",
+        postEvent: "Tugce Nur returns to Office",
+        postDes: "We will continue working on this app together on Tuesday!",
+      }
+    ]
+  },
+  methods{
+
+    dataFuncs(): {
+    let currentDate = new Date();
+    let cDay = currentDate.getDate();
+    let cMonth = currentDate.getMonth() + 1;
+    let cYear = currentDate.getFullYear();
+    
+  }
+  }
+};
+
+-->
 <!--Scripts-->
 <script>
 export default {
   name: "OpportunityWelcomeScreen",
-
+  
   data() {
+
+    
     let currentDate = new Date();
     let cDay = currentDate.getDate();
     let cMonth = currentDate.getMonth() + 1;
     let cYear = currentDate.getFullYear();
     return {
       date: cDay + ".0" + cMonth + "." + cYear,
+      allPosts: [
+      {
+        postDate: "14 00 Uhr 07.08.2021",
+        postEvent: "Basisbeschäftigung Besuch",
+        postDes: "Interessierte für den zweiten Kurs werden uns besuchen",
+      },
+      {
+        postDate: "15 30 Uhr 09.08.2021",
+        postEvent: "Firmenbesuch Microsoft",
+        postDes: "Roger Halbheer, Head of Security",
+      },
+      {
+        postDate: "8 30 Uhr 10.08.2021",
+        postEvent: "Tugce Nur returns to Office",
+        postDes: "We will continue working on this app together on Tuesday!",
+      }
+    ]
     };
   },
 };
@@ -61,7 +106,7 @@ export default {
 
 <!--Style-->
 <style>
-
+@import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
 /* App / Center-Class / Main Container / Footer*/
 #app {
   font-family: Arial, Helvetica, sans-serif;
@@ -73,6 +118,7 @@ export default {
 }
 
 .center {
+  font-family: "Inter";
   margin: auto;
   width: 50%;
   padding: 10px;
@@ -115,11 +161,10 @@ export default {
   font-weight: 100;
 }
 
-
 /*Logos*/
 
 .logo2 {
-  width: 200px;
+  width: 180px;
   margin: 50px;
   text-align: center;
   vertical-align: middle;
@@ -128,7 +173,6 @@ export default {
 .logopos {
   transform: translateY(20%);
 }
-
 
 .logo {
   display: block;
@@ -149,6 +193,48 @@ export default {
   line-height: 150px;
   padding: 20px;
   width: auto;
+  animation: fadeOutUp 0.5s ease-in forwards;
+}
+
+.list li:hover {
+  list-style-type: none;
+  background-color: rgb(77, 71, 190);
+  font-size: 30px;
+  color: rgb(255, 255, 255);
+  margin: 50px;
+  line-height: 150px;
+  padding: 20px;
+  width: auto;
+  animation: fadeInDown 0.5s ease-in forwards;
+}
+
+@keyframes fadeInDown {
+  from {
+    transform: translate(0, 0%);
+  }
+  to {
+    transform: translate(0, -10%);
+  }
+}
+
+@keyframes fadeOutUp {
+  from {
+    transform: translate(0, -10%);
+  }
+  to {
+    transform: translate(0, 0%);
+  }
+}
+
+@keyframes fadeInSide {
+  from {
+    opacity: 0;
+    transform: translate(-100%, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
 }
 
 p {
